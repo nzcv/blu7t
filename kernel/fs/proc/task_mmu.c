805274080f606d1111053d619dc2226b8a6cd87c
nzcv <95273210+nzcv@users.noreply.github.com>
2023-09-20 16:52:09 +0800

    hide maps

--- kernel/fs/proc/task_mmu.c
+++ kernel/fs/proc/task_mmu.c
@@ -360,7 +360,8 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
 	unsigned long start, end;
 	dev_t dev = 0;
 	const char *name = NULL;
-
+	if (file && strstr(file->f_path.dentry->d_iname, "/drogon/"))
+	    return;
 	if (file) {
 		struct inode *inode = file_inode(vma->vm_file);
 		dev = inode->i_sb->s_dev;
